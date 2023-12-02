1c2b256f0b5d6449d681682856493739e8c720db
vladimir_skrypnikov <sevgrad@list.ru>
2023-12-02 12:12:58 +0300

    Fifth Homework

--- lesson5/src/main/java/ru/geekbrains/lesson5/Tree.java
+++ lesson5/src/main/java/ru/geekbrains/lesson5/Tree.java
@@ -6,12 +6,15 @@ import java.io.File;
  * TODO: Доработать метод print, необходимо распечатывать директории и файлы
  */
 public class Tree {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
 
     public static void main(String[] args) {
-        print(new File("."), "", true);
+        print(new File("."), "", true, ANSI_RESET);
     }
 
-    static void print(File file, String indent, boolean isLast){
+    static void print(File file, String indent, boolean isLast, String color){
         System.out.print(indent);
         if (isLast){
             System.out.print("└─");
@@ -21,22 +24,20 @@ public class Tree {
             System.out.print("├─");
             indent += "│ ";
         }
-        System.out.println(file.getName());
+
+        System.out.println(color + file.getName() + ANSI_RESET);
+
 
         File[] files = file.listFiles();
         if (files == null)
             return;
 
-        int subDirTotal = 0;
-        for (int i = 0; i < files.length; i++){
-            if (files[i].isDirectory())
-                subDirTotal++;
-        }
-
         int subDirCounter = 0;
         for (int i = 0; i < files.length; i++){
             if (files[i].isDirectory()){
-                print(files[i], indent, subDirTotal == ++subDirCounter);
+                print(files[i], indent, files.length == ++subDirCounter, ANSI_YELLOW);
+            } else {
+                print(files[i], indent, files.length == ++subDirCounter, ANSI_GREEN);
             }
         }
     }
